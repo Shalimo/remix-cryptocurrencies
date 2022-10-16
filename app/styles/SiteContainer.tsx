af119a4d222f5d06abbd08180b06730ed83364a0
Nikita Shalimo <shpoisnik@gmail.com>
2022-10-16 22:07:07 +0300

    add font for site

--- app/styles/SiteContainer.tsx
+++ app/styles/SiteContainer.tsx
@@ -1,10 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Signika+Negative:wght@300&display=swap');
   body {
   color: #000;
   background: #e6e6fa;
   background-repeat: repeat;
+  font-family: 'Signika Negative', sans-serif;
   }
 `;
 export default GlobalStyle;
